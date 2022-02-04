66a3b0c79fe6971345fb870bf3fe0d8d07b54f4e
LeslieRichardCheung <98405581+LeslieRichardCheung@users.noreply.github.com>
2022-02-04 18:50:12 +0800

    Add files via upload

--- uploadSystem/index.php
+++ uploadSystem/index.php
@@ -1,1 +1,32 @@
-
+<style>
+    a,a:link,a:visited{
+        color:blue;
+    }
+</style>
+<div>
+<?php
+function ls($url = null) //used to list all files in a folder
+{
+    error_reporting(0); //didn't call warning
+    $num = 0; //the number of files
+    $dir = opendir($url);
+    while (false !== ($file = readdir($dir))) {
+        if ($file == '.' || $file == '..'||$file=='index.php'||$file=='upload.php'||$file=='play.php') {
+            continue;
+        }
+        echo <<<EOF
+        <div><a href="play.php?fn=$file" target='_blank'>$file</a><br/><a href="$file" download>下载</a></div>\n
+        EOF;
+    }
+    closedir($dir);
+    return $num;
+}
+ls("./")
+?>
+</div>
+<div>
+    <form method='post' action='upload.php' enctype="multipart/form-data">
+        <input type='file' name='fn'/>
+        <input type='submit'>
+    </form>
+</div>
---
+++ uploadSystem/play.php
@@ -0,0 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+<body style='text-align:center'>
+    <?php echo "<h1>".$_GET['fn']."</h1>\n<iframe src='".$_GET['fn']."'></iframe>"; ?>
+</body>
+</html>
